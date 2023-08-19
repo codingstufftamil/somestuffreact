54004f6ab8278a7520a1a1f305062abf7d517665
codingstuff <codingstufftamil@gmail.com>
2023-08-19 14:42:33 +0530

    'removed_unused_imports'

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,9 @@
-import { Link } from "react-router-dom";
+
 
 function Home() {
   return (
    <div style={{overflow: 'hidden',height:'100vh'}}>
-<iframe class='framesd' style={{width:'100vw',height:'100vh'}} src="https://somestuff.netlify.app/"></iframe>
+<iframe classname='framesd' style={{width:'100vw',height:'100vh'}} title='somestuff' src="https://somestuff.netlify.app/"></iframe>
    </div>
   );
 }
