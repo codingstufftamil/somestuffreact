e06800d997838c75ce7b5183eea256557c30cf9f
codingstuff <codingstufftamil@gmail.com>
2023-08-19 14:36:51 +0530

    'reactbuild_with_iframess'

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+   <div style={{overflow: 'hidden',height:'100vh'}}>
+<iframe class='framesd' style={{width:'100vw',height:'100vh'}} src="https://somestuff.netlify.app/"></iframe>
+   </div>
+  );
+}
+ 
+export default Home;
